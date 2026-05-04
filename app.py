380a68dae3b69abb6e3f1dac35921664d45034f6
ReonRodrigues <reonprinstonrodrigues@gmail.com>
2026-05-04 12:43:38 +0530

    third commit

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def predict():
     return render_template("predict.html", prediction_text=prediction_text)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host="0.0.0.0")
